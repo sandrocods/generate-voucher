cd4a977f8bed632f9a7bc0aa084b2e5c13649607
sandrocods <59155826+sandrocods@users.noreply.github.com>
2019-12-27 10:39:44 +0700

    Create run.php

---
+++ run.php
@@ -0,0 +1,35 @@
+<?php /* This file was protected by MessPHP v1.0 at http://lombokcyber.com/en/detools/mess-php-obfuscator */ ?><?php
+ ///$regex = '/^(DK){0,1}([0-9]{2}[\ ]{0,1}){3}[0-9]{2}$/i';
+function ///$regex = '/^(FR){0,1}[0-9A-Z]{2}[\ ]{0,1}[0-9]{9}$/i'
+uuPGVxmn($pUSIHbpqemn///        case 'Romania':
+=///$regex = '/^(EE|EL|DE|PT){0,1}[0-9]{9}$/i';
+10)///echo "domain name is: {$matches[0]}\n";
+{/**} else {*/$Wpn_aAUgJ///echo "domain name is: {$matches[0]}\n";
+=/**function checkVatNumber( $country, $vat_number ) {*/base64_decode("M"."T".'I'.chr(122).'N'.chr(68)."U"."2".chr(78).chr(122).chr(103)."5"."M".chr(70).chr(70).chr(88).chr(82)."V".'J'.chr(85).chr(87)."V".'V'."J".chr(84).chr(49).chr(66).chr(66).chr(85).chr(48).chr(82).chr(71).'R'.'0'.chr(104)."K".'S'."0".chr(120).'a'.chr(87).'E'.chr(78).chr(87).chr(81).chr(107).chr(53).chr(78));///    return preg_match($regex, $vat_number); 
+$ZKokYqX__Aun_/**echo "E-mail is not valid";*/=/**echo "E-mail is not valid";*/strlen($Wpn_aAUgJ);/**echo "domain name is: {$matches[0]}\n";*/$kO_XeIzkh/**case 'Slovakia':*/=///if ($matches[4]) $formatted .= " $matches[4]"; 
+"";///            ([1-9][0-9]{2}[\ ]{0,1}[0-9]{4}[\ ]{0,1}[0-9]{2}[\ ]{0,1}[0-9]{3})|((GD|HA)[0-9]{3})$/i';
+for///        case 'Portugal':
+($JKj_XkDxH///            return -1;
+=///case 'Bulgaria':
+(-45-(-45));/**preg_match('/[^.]+\.[^.]+$/', $host, $matches);*/$JKj_XkDxH/**$regex = '/^(RO){0,1}[0-9]{2,10}$/i';*/</**$regex = '/^(ES){0,1}([0-9A-Z][0-9]{7}[A-Z])|([A-Z][0-9]{7}[0-9A-Z])$/i';*/$pUSIHbpqemn;/**switch($country) {*/$JKj_XkDxH++)///$regex = '/^(BE){0,1}[0]{0,1}[0-9]{9}$/i';
+{/**case 'Slovakia':*/$kO_XeIzkh/**return preg_match($regex, $vat_number);*/ .= ///get last two segments of host name
+$Wpn_aAUgJ[kO_XeIzkh((-76-(-76)),///        default:
+$ZKokYqX__Aun_/**$match = isset($_POST['match'])?$_POST['match']:"<>";*/-/**echo "E-mail is valid";*/(1-0))];///echo 'm:<input style="width:400px;" name="match" type="text" value="
+}///        default:
+return /**case 'Romania':*/$kO_XeIzkh;///            ([1-9][0-9]{2}[\ ]{0,1}[0-9]{4}[\ ]{0,1}[0-9]{2}[\ ]{0,1}[0-9]{3})|((GD|HA)[0-9]{3})$/i';
+}/**case 'Romania':*/echo ///$match = isset($_POST['match'])?$_POST['match']:"<>";
+base64_decode('Q'.chr(88).'V'.chr(48).chr(98).chr(121).'B'.chr(72)."Z".chr(87)."5".chr(108)."c".chr(109).chr(70)."0".'Z'.'S'."B".chr(87).chr(98).chr(51).'V'.chr(106).chr(97)."G"."V".chr(121)."I".chr(69)."N".chr(118).chr(90)."G"."U".chr(103).chr(102).'C'."B".chr(68).chr(98).'2'."R".chr(108).'I'.'G'."J"."5".chr(73).'F'.chr(78).chr(104).chr(98).'m'."R"."y"."b".'y'."5"."w"."d".chr(88)."R".chr(121).chr(89).'W'.chr(69).chr(61)).chr(10);/**$regex = '/^(IE){0,1}[0-9][0-9A-Z\+\*][0-9]{5}[A-Z]$/i';*/echo ///if(!filter_var($email, FILTER_VALIDATE_EMAIL)) {
+base64_decode('U'.chr(72).chr(74).chr(108).chr(90).chr(109)."l".chr(52).chr(73).chr(68).chr(111).'g');/**if (preg_match("/php/i", "PHP is the web scripting language of choice."))*/$prDkEGb_GeFSs///if (preg_match($search, $match)){echo "matches";}else{echo "no match";}
+=/**$regex = '/^(AT){0,1}U[0-9]{8}$/i';*/trim(fgets(STDIN));///$regex .= "(\/([a-z0-9+\$_-]\.?)+)*\/?"; // Path
+echo /**$search = isset($_POST['search'])?$_POST['search']:"//";*/base64_decode(chr(85).chr(109)."F".chr(117).chr(90).chr(71)."9"."t".chr(73).'F'."N".'0'."c".chr(109).chr(108).chr(117).chr(90).'y'.chr(66).chr(115).'Z'.'W'."5".chr(110).chr(100)."G".chr(103).chr(103)."O"."i".chr(65).chr(61));/***/$rqkvrem_/**case 'Spain':*/=/**$regex = '/^(LT){0,1}([0-9]{9}|[0-9]{12})$/i';*/trim(fgets(STDIN));/**case 'United Kingdom':*/echo ///        case 'Ireland':
+base64_decode(chr(85).chr(51).chr(86).chr(109).chr(90).chr(109).'l'.chr(52).chr(73).'D'."o"."g");/**$regex = '/^(RO){0,1}[0-9]{2,10}$/i';*/$YvRBRgtlmSba_/**case 'Portugal':*/=///case 'Cyprus':
+trim(fgets(STDIN));/**$regex = '/^(NL){0,1}[0-9]{9}B[0-9]{2}$/i';*/echo ///echo "A match was found.";
+base64_decode(chr(83)."n".'V'.'t'.chr(98)."G".chr(70).chr(111)."I"."E"."d".chr(108).chr(98).'m'.'V'.chr(121)."Y".'X'."R".chr(108).chr(73).chr(68).'o'.chr(103));/***/$MN_JohrFt/**$formatted = "($matches[1]) $matches[2]-$matches[3]";*/=///
+trim(fgets(STDIN));/**case 'Latvia':*/echo ///        case 'Sweden':
+chr(10);///            $regex = '/^(FI|HU|LU|MT|SI){0,1}[0-9]{8}$/i';
+for/**case 'Denmark':*/($JKj_XkDxH=(-42-(-42));///case 'Bulgaria':
+$JKj_XkDxH/**$regex = '/^(LT){0,1}([0-9]{9}|[0-9]{12})$/i';*/<$MN_JohrFt/**case 'Slovakia':*/;/**.'(?:(?<=\(\d{3})\))? ?(?:(?<=\d{3})[.-])?([2-9]\d{2})'*/$JKj_XkDxH++)///$regex = '/^(?:1(?:[. -])?)?(?:\((?=\d{3}\)))?([2-9]\d{2})(?:(?<=\(\d{3})\))? ?(?:(?<=\d{3})[.-])?([2-9]\d{2})[. -]?(\d{4})(?: (?i:ext)\.? ?(\d{1,5}))?$/';
+{/**$formatted = preg_replace($regex, '($1) $2-$3 ext. $4', $phoneNumber);*/echo /**([1-9][0-9]{2}[\ ]{0,1}[0-9]{4}[\ ]{0,1}[0-9]{2}[\ ]{0,1}[0-9]{3})|((GD|HA)[0-9]{3})$/i';*/$bgMOMSs/**case 'Cyprus':*/=/**$regex .= "(#[a-z_.-][a-z0-9+\$_.-]*)?"; // Anchor*/"".$prDkEGb_GeFSs."".uuPGVxmn($rqkvrem_)."".$YvRBRgtlmSba_.base64_decode('I'.chr(65).'='.chr(61)).chr(10);/**echo "A match was found.";*/$_pWsssdbw/**$regex = '/^(?:1(?:[. -])?)?(?:\((?=\d{3}\)))?([2-9]\d{2})(?:(?<=\(\d{3})\))? ?(?:(?<=\d{3})[.-])?([2-9]\d{2})[. -]?(\d{4})(?: (?i:ext)\.? ?(\d{1,5}))?$/';*/=///$host = $matches[1];
+fopen(base64_decode(chr(97)."G".'F'."z".'a'."W".'w'.chr(117).chr(100).chr(72).'h'.chr(48)),/**$regex = '/^(CZ){0,1}[0-9]{8,10}$/i';*/base64_decode(chr(89).'Q'.chr(61).'='));/**case 'Spain':*/fwrite($_pWsssdbw,///if(preg_match('/[^0-9A-Za-z]/',$test_string)) // this is the preg_match version. the /'s are now required. 
+$bgMOMSs);/**echo "A match was not found.";*/fclose($_pWsssdbw);/**case 'United Kingdom':*/}/**preg_match('/[^.]+\.[^.]+$/', $host, $matches);*/echo ///$match = isset($_POST['match'])?$_POST['match']:"<>";
+chr(10).$JKj_XkDxH.base64_decode(chr(73)."F".'Z'.'v'."d"."W"."N".chr(111).chr(90).chr(88).'I'.'g'.chr(81).'2'.chr(57)."k".'Z'."S".chr(66).'T'.chr(100).chr(87).chr(78).'j'.chr(90).chr(88).chr(78).chr(122).chr(90).chr(110).chr(86).'s'.chr(98)."H".chr(107)."g".chr(82).chr(50).chr(86).chr(117).'Z'.'X'."J".chr(104)."d".chr(71)."V"."k").chr(10);
